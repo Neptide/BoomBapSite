05873b49297a5dec6d65e53524dbd2d816d7a327
fabrizio <fabriziofiorettiinc@gmail.com>
2023-11-26 15:53:15 +0100

    Primo commit

---
+++ public/script.js
@@ -0,0 +1,17 @@
+document.getElementById('ticketForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var formData = new FormData(event.target);
+    fetch('/create-ticket', {
+        method: 'POST',
+        body: formData
+    }).then(response => response.blob())
+      .then(blob => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = 'biglietto.pdf';
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+      }).catch(err => console.error(err));
+});
